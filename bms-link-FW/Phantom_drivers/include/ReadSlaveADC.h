3d847cbdbb2338b292e41e20d9dc9a60aa7e9af0
Tsidhu15 <tanjosh.sidhu@gmail.com>
2026-01-17 13:45:44 -0800

    untested Code, looks like it should work, nothing in main

--- bms-link-FW/Phantom_drivers/include/ReadSlaveADC.h
+++ bms-link-FW/Phantom_drivers/include/ReadSlaveADC.h
@@ -15,32 +15,18 @@ Author: Tanjosh Sidhu
 //dataconfig1_t.CSNR    = 0xFE;
 
 
-#define NumberOfSlaves = 1;
-uint8_t Ref_ON = 0;
-
-#define ADC_Voltage_cmd [(LTC6811_ADCV >> 8) & 0x00ff, LTC6811_ADCV & 0x00ff]
-
+#define NumberOfSlaves  1
 
 
 void uint16_t2ByteArray(uint16_t in, uint8_t* out);
 uint16_t ByteArray2uint16_t(uint8_t* in);
 
-bool setupACD_init();
-
-void wakeup_idle(uint8_t total_ic);
+bool setupACD_init(uint8_t total_ic);
 void wakeup_sleep(uint8_t total_ic);
+bool SetRefOn(bool NewState);
+void readAllADC(uint16_t cmd, uint16_t* data, uint8_t total_ic);
 
-void MasterCommandAllSlaves(uint16_t cmd, uint16_t* data, uint8_t NumOfSlaves, spiDAT1_t dataconfig);
-//
-//bool setupACD_init();
-//bool WakeupYourSlaves_ADC()
-//bool ProcessData(uint16_t* Raw, uint16_t* OutPut, uint8_t data_len);
-//bool MasterCommandAllYourSlaves(uint16_t cmd, uint16_t* data, uint8_t NumOfSlaves, spiDAT1_t dataconfig);
-//
-//int SaveData(uint16_t* Disnation, uint16_t* data);
-//
-//
-//
-//bool Sleep2Standby();
-//
-//uint8_t* uint16_t2ByteArray(uint16_t TheBytes);
+
+void WipTheSlavesAwake(uint8_t NumOfSlaves);
+void buySlaves(uint8_t NumOfSlaves);
+void MasterCommandAllSlaves(uint16_t cmd, uint16_t* data, uint8_t NumOfSlaves);
--- bms-link-FW/Phantom_drivers/source/ReadSlaveADC.c
+++ bms-link-FW/Phantom_drivers/source/ReadSlaveADC.c
@@ -24,27 +24,73 @@ uint16_t ByteArray2uint16_t(uint8_t* in){
     return out;
 }
 
-bool SetRefOn(bool State){
+bool setupACD_init(uint8_t total_ic){
+    #define NumOfBytes2Config 6
+    uint8_t CFGA[NumOfBytes2Config] = {0};
+//    CFGR[0] = 0b00000100;
+//    CFGR[0] = 1<<2;
 
+    wakeup_sleep(total_ic);
+
+    return write_reg(LTC6811_WRCFGA, CFGA, NumOfBytes2Config);
+}
+
+void wakeup_sleep(uint8_t total_ic){
+    #define DummySize 1
+
+    const uint16_t dummyCmd = LTC6811_RDCFGA;
+    uint8_t AllSlaveOut[DummySize];
+    int i;
+    for(i=0; i<total_ic; i++)
+        read_reg(dummyCmd, &AllSlaveOut[0], DummySize);
 }
 
 
 
-void MasterCommandAllSlaves(uint16_t cmd, uint16_t* dataOut, uint8_t NumOfSlaves, spiDAT1_t dataconfig){
-    const uint8_t UnitSize_Shift = 1;
-    const uint8_t UnitSize = 1<<UnitSize_Shift;
-    uint8_t cmdArr[UnitSize];
+bool SetRefOn(bool NewState){
+    const uint8_t dataLen = 1;
+    const uint8_t RefOnBit = 2;
+
+    uint8_t Data2Write = NewState? (1<<RefOnBit): 0;
+
+    return write_reg(LTC6811_WRCFGA, &Data2Write, dataLen);
+}
+
+void readAllADC(uint16_t cmd, uint16_t* dataOut, uint8_t total_ic){
+    #define UnitSize        2
 
     uint8_t AllSlaveOut[UnitSize];
 
-    bytes2ByteArray(cmd, &cmdArr[0]);
+    SetRefOn(1);
+
+    int i=0;
+    for (i=0; i<total_ic; i++){
+        read_reg(cmd, &AllSlaveOut[0], UnitSize);
+
+        dataOut[i] = ByteArray2uint16_t(&AllSlaveOut[0]);
+    }
+    SetRefOn(0);
+}
+
+
+void buySlaves(uint8_t NumOfSlaves){
+    setupACD_init(NumberOfSlaves);
+}
+void WipTheSlavesAwake(uint8_t NumOfSlaves){
+    wakeup_sleep(NumOfSlaves);
+}
+void MasterCommandAllSlaves(uint16_t cmd, uint16_t* dataOut, uint8_t NumOfSlaves){
+    #define UnitSize        2
+
+    uint8_t AllSlaveOut[UnitSize];
 
     SetRefOn(1);
 
     int i=0;
     for (i=0; i<NumOfSlaves; i++){
-        spiSendAndGetData(spiREG3, &dataconfig1_t, UnitSize, &cmdArr[0], &AllSlaveOut[0]);
-        uint16_t2ByteArray(&AllSlaveOut[0], dataOut[i]);
+        read_reg(cmd, &AllSlaveOut[0], UnitSize);
+
+        dataOut[i] = ByteArray2uint16_t(&AllSlaveOut[0]);
     }
     SetRefOn(0);
 }
