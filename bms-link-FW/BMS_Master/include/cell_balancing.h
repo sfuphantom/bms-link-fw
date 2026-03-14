4f584c4aa46f0c44ba1e5d9f2bc0d4f2ccec5bb8
Joseph Wen <josephwen327@gmail.com>
2026-03-14 09:11:26 -0700

    feat: add initial files

---
+++ bms-link-FW/BMS_Master/include/cell_balancing.h
@@ -0,0 +1,13 @@
+/*
+ * cell_balancing.h
+ *
+ *  Created on: Mar 14, 2026
+ *      Author: joeyw
+ */
+
+#ifndef BMS_MASTER_INCLUDE_CELL_BALANCING_H_
+#define BMS_MASTER_INCLUDE_CELL_BALANCING_H_
+
+
+
+#endif /* BMS_MASTER_INCLUDE_CELL_BALANCING_H_ */
---
+++ bms-link-FW/BMS_Master/source/cell_balancing.c
@@ -0,0 +1,16 @@
+/*
+ * cell_balancing.c
+ *
+ *  Created on: Mar 14, 2026
+ *      Author: joeyw
+ */
+
+
+#include "cell_balancing.h"
+
+//Random values atm, check
+#define NUM_CELLS 16 //double check
+#define BAL_THRESHOLD 10
+#define BAL_MIN 3000
+#define BAL_MAX 4200
+
