452474c828889681d1ac13a3ffda5b7259248ec1
Joseph Wen <josephwen327@gmail.com>
2026-03-21 13:41:53 -0700

    feat: Implement initial algorithm

--- bms-link-FW/BMS_Master/include/cell_balancing.h
+++ bms-link-FW/BMS_Master/include/cell_balancing.h
@@ -8,6 +8,29 @@
 #ifndef BMS_MASTER_INCLUDE_CELL_BALANCING_H_
 #define BMS_MASTER_INCLUDE_CELL_BALANCING_H_
 
+#include <stdint.h>
+#include <stdbool.h>
 
+//Random values atm, check
+#define NUM_CELLS 16 //double check
+#define BAL_THRESHOLD 10
+#define BAL_MIN 3000
+#define BAL_MAX 4200
+
+#define MOSFET_ON 1
+#define MOSFET_OFF 0
+
+typedef enum {
+    BAL_OK = 0,
+    BAL_ERR_PARAM = -1,
+    BAL_ERR_FAULT = -2,
+
+} BalanceStatus_t;
+
+
+BalanceStatus_t BalanceCycle(uint16_t *voltages, uint8_t num_cells, bool *switches);
+
+void MOSFET_EN(bool EN, int cell);
+void check_cells();
 
 #endif /* BMS_MASTER_INCLUDE_CELL_BALANCING_H_ */
--- bms-link-FW/BMS_Master/source/cell_balancing.c
+++ bms-link-FW/BMS_Master/source/cell_balancing.c
@@ -7,10 +7,55 @@
 
 
 #include "cell_balancing.h"
+#include <string.h>
 
-//Random values atm, check
-#define NUM_CELLS 16 //double check
-#define BAL_THRESHOLD 10
-#define BAL_MIN 3000
-#define BAL_MAX 4200
+
+BalanceStatus_t BalanceCycle(uint16_t *voltages, uint8_t num_cells, bool *switches){
+    //Base Check
+    if(voltages == NULL || switches == NULL || num_cells == 0 || num_cells > NUM_CELLS)
+        return BAL_ERR_PARAM
+
+    memset(switches, 0, num_cells * sizeof(bool));  //Set all switches off
+
+    //Look for lowest voltage cell
+    uint16_t v_min = UINT16_MAX;
+    for(int i = 0; i < num_cells; i++){
+        uint16_t v = voltages[i];
+        if(v < BAL_MIN || v > BAL_MAX)
+            return BAL_ERR_FAULT;
+        if(v < v_min)
+            v_min = v;
+    }
+
+    for(int i = 0; i < num_cells; i++){
+        if (voltages[i] > v_min + BALANCE_THRESHOLD_MV)
+            switches[i] = true;
+        else
+            switches[i] = false;
+    }
+
+    return BAL_OK
+}
+
+
+void MOSFET_EN(bool EN, int cell){
+    if(MOSFET_ON){
+        //enable gpio of cell
+    }
+    else{
+        //turn off gpio of cell
+    }
+}
+
+void check_cells(){
+    for(int i = 0; i < NUM_CELLS; i++){
+        if(cell_voltage[i] > BAL_THRESHOLD){
+            MOSFET_EN(MOSFET_ON, i);
+        }
+        else{
+            MOSFET_EN(MOSFET_OFF, i);
+        }
+    }
+}
+
 
